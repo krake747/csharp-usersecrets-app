7e33e3b0997a1acc83e003f167097d14f8a8917e
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-02-26 23:05:38 +0100

    🐛 bug(cryptography): fixed hash and verify methods

--- UserSecretsApp/Program.cs
+++ UserSecretsApp/Program.cs
@@ -26,22 +26,23 @@ var verified = password.Verify(hash, salt, hashAlgorithm, keySize, iterations);
 
 Console.WriteLine($"Password is {(verified ? "verified" : "not verified")}");
 
-Console.WriteLine("Bye UserSecrets App");
+Console.WriteLine("Bye User Secrets App!");
 
 
 internal record Password(string Value)
 {
     internal (string Hash, byte[] Salt) Hash(HashAlgorithmName hashAlgorithm, int keySize, int iterations)
     {
+        var password = Encoding.UTF8.GetBytes(Value);
         var salt = RandomNumberGenerator.GetBytes(keySize);
-        var hash = Rfc2898DeriveBytes.Pbkdf2(Value, salt, iterations, hashAlgorithm, keySize);
+        var hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, iterations, hashAlgorithm, keySize);
         return (Convert.ToHexString(hash), salt);
     }
 
     internal bool Verify(string hash, byte[] salt, HashAlgorithmName hashAlgorithm, int keySize,
         int iterations)
     {
-        var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(hash, salt, iterations, hashAlgorithm, keySize);
+        var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(Value, salt, iterations, hashAlgorithm, keySize);
         return hashToCompare.SequenceEqual(Convert.FromHexString(hash));
     }
 }
