6527760488ab332ba717f95efc60b0344f525511
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-03-01 22:02:57 +0100

    ✨ feat(vault): use clipboard service

--- UserSecretsApp/Program.cs
+++ UserSecretsApp/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using Microsoft.Extensions.Configuration;
+using TextCopy;
 
 var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
@@ -12,6 +14,16 @@ var hashAlgorithm = new HashAlgorithmName(config["HashAlgorithm"]);
 var iterations = Convert.ToInt32(config["Iterations"]);
 var keyLength = Convert.ToInt32(config["KeyLength"]);
 var encryptionService = new EncryptionService(hashAlgorithm, iterations, keyLength);
+
+var s = new Secret("kk", "hh");
+try
+{
+    Console.WriteLine(s.ToString());
+}
+catch (Exception ex)
+{
+    // ignored
+}
 
 if (config["MasterHash"] == string.Empty)
 {
@@ -24,48 +36,58 @@ if (config["MasterHash"] == string.Empty)
     Environment.Exit(0);
 }
 
+Console.WriteLine("Welcome to the UserSecrets Vault!");
+Console.WriteLine();
 var masterSecret = ReadLine("Enter Master password: ", Console.Write);
 
 var masterHash = EncryptionService.BitConverterToBytes(config["MasterHash"]!);
 var masterSalt = EncryptionService.BitConverterToBytes(config["MasterSalt"]!);
 
 var verifiedMasterSecret = encryptionService.VerifyHash(masterSecret, masterHash, masterSalt);
-var curDir = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\Vault\"));
-var userSecretsFileInfo = new FileInfo(Path.Combine(curDir.FullName, "user-secrets.json"));
+
+if (!verifiedMasterSecret)
+{
+    Console.WriteLine("Unable to verify Master Secret!");
+    Environment.Exit(0);
+}
+
+var currentDir = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\Vault\"));
+var userSecretsFileInfo = new FileInfo(Path.Combine(currentDir.FullName, "secrets-container.json"));
 
 if (!userSecretsFileInfo.Exists)
 {
     userSecretsFileInfo.Create().Dispose();
 }
 
-var json = File.ReadAllText(userSecretsFileInfo.FullName);
-var secretContainers = json == string.Empty
-    ? new List<SecretContainer>()
-    : JsonSerializer.Deserialize<List<SecretContainer>>(json)!;
+var json = await File.ReadAllTextAsync(userSecretsFileInfo.FullName);
+var secretContainers = json != string.Empty
+    ? JsonSerializer.Deserialize<List<SecretContainer>>(json)! 
+    : new List<SecretContainer>();
 
 var exit = false;
 while (verifiedMasterSecret && userSecretsFileInfo.Exists && !exit)
 {
-    Console.WriteLine("Welcome to the UserSecrets Vault!");
-
     var key = ReadLine("Enter User Key: ", Console.Write);
     var secretContainer = secretContainers.FirstOrDefault(u => u.Key == key);
 
     if (secretContainer is null)
     {
+        Console.WriteLine();
         Console.WriteLine($"User key - {key} - not found.");
         var newKey = ReadLine("Enter New User Key: ", Console.Write);
         var newName = ReadLine("Enter New User Name: ", Console.Write);
         var newPassword = ReadLine("Enter New User Password: ", Console.Write);
         var newSecret = new Secret(newName, newPassword);
+        var newJsonSecret = JsonSerializer.Serialize(newSecret); 
 
         var rngSalt = RandomNumberGenerator.GetBytes(keyLength);
         var salt = EncryptionService.BitConverterToString(rngSalt);
-        var encrypted = await encryptionService.EncryptAsync(newSecret.ToString(), newKey, salt);
+        var encrypted = await encryptionService.EncryptAsync(newJsonSecret, newKey, salt);
         var newEncryptedSecret = EncryptionService.BitConverterToString(encrypted);
         var newSecretContainer = new SecretContainer(newKey, salt, newEncryptedSecret);
         Console.WriteLine("New Secret Container added...");
-        Console.WriteLine($"Encrypted data: {newEncryptedSecret}");
+        Console.WriteLine();
+        //Console.WriteLine($"Encrypted data: {newEncryptedSecret}");
 
         secretContainers.Add(newSecretContainer);
         continue;
@@ -73,21 +95,29 @@ while (verifiedMasterSecret && userSecretsFileInfo.Exists && !exit)
 
     var decryptedSecret = EncryptionService.BitConverterToBytes(secretContainer.EncryptedSecret);
     var decrypted = await encryptionService.DecryptAsync(decryptedSecret, secretContainer.Key, secretContainer.Salt);
-    Console.WriteLine($"Decrypted data: {decrypted}");
+    var secret = JsonSerializer.Deserialize<Secret>(decrypted);
+    //Console.WriteLine($"Decrypted data: {decrypted}");
 
-    Console.Write("Exit: (y/n)");
+    if (secret is not null)
+    {
+        await ClipboardService.SetTextAsync(secret.Password);
+    }
+    
+    Console.WriteLine("Exit: (y/n)");
     exit = Console.ReadKey(true).Key is ConsoleKey.Y;
 }
 
-// var rngSalt = RandomNumberGenerator.GetBytes(keyLength);
-// var salt = EncryptionService.BitConverterToString(rngSalt);
-// var secret = new Secret("kk@email.com", "hh");
-// var secretContainer = new SecretContainer("kk", salt, secret);
-// var encrypted = await encryptionService.EncryptAsync(secret.ToString(), secretContainer.Key, secretContainer.Salt);
-// Console.WriteLine($"Encrypted data: {BitConverter.ToString(encrypted)}");
-//
-// var decrypted = await encryptionService.DecryptAsync2(encrypted, secretContainer.Key, secretContainer.Salt);
-// Console.WriteLine($"Decrypted data: {decrypted}");
+var options = new JsonSerializerOptions 
+{ 
+    WriteIndented = true
+};
+
+var jsonString = JsonSerializer.Serialize(secretContainers, options);
+
+await Task.Run(() => File.WriteAllText(userSecretsFileInfo.FullName, jsonString));
+
+Console.WriteLine("Saving file...");
+
 Console.WriteLine();
 
 static string ReadLine(string? message, Action<string> writer)
