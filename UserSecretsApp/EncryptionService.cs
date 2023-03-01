a4376929475f0271d8e2fbad164938cb72b47fd4
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-03-01 22:16:55 +0100

    ✨ feat(service): move EncryptionService to new file

---
+++ UserSecretsApp/EncryptionService.cs
@@ -0,0 +1,75 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace UserSecretsApp;
+
+internal class EncryptionService
+{
+    private readonly HashAlgorithmName _hashAlgorithm;
+    private readonly int _iterations;
+    private readonly int _keyLength;
+
+    public EncryptionService(HashAlgorithmName hashAlgorithm, int iterations, int keyLength)
+    {
+        _hashAlgorithm = hashAlgorithm;
+        _iterations = iterations;
+        _keyLength = keyLength;
+    }
+
+    internal async Task<byte[]> EncryptAsync(string message, string secretKey, string salt)
+    {
+        using var aes = Aes.Create();
+        aes.Key = CreateDerivedKey(secretKey, Array.Empty<byte>(), _hashAlgorithm, _iterations, _keyLength);
+        aes.IV = BitConverterToBytes(salt);
+        var encryptor = aes.CreateEncryptor();
+        using var output = new MemoryStream();
+        await using var cryptoStream = new CryptoStream(output, encryptor, CryptoStreamMode.Write);
+        await cryptoStream.WriteAsync(Encoding.Unicode.GetBytes(message));
+        await cryptoStream.FlushFinalBlockAsync();
+        return output.ToArray();
+    }
+
+    internal async Task<string> DecryptAsync(byte[] encrypted, string secretKey, string salt)
+    {
+        using var aes = Aes.Create();
+        aes.Key = CreateDerivedKey(secretKey, Array.Empty<byte>(), _hashAlgorithm, _iterations, _keyLength);
+        aes.IV = BitConverterToBytes(salt);
+        var decryptor = aes.CreateDecryptor();
+        using var input = new MemoryStream(encrypted);
+        await using var cryptoStream = new CryptoStream(input, decryptor, CryptoStreamMode.Read);
+        using var output = new MemoryStream();
+        await cryptoStream.CopyToAsync(output);
+        return Encoding.Unicode.GetString(output.ToArray());
+    }
+
+    private static byte[] CreateDerivedKey(string secretKey, byte[] salt, HashAlgorithmName hashAlgorithm,
+        int iterations, int keyLength)
+    {
+        var secret = Encoding.Unicode.GetBytes(secretKey);
+        return Rfc2898DeriveBytes.Pbkdf2(secret, salt, iterations, hashAlgorithm, keyLength);
+    }
+
+    internal (byte[] Hash, byte[] Salt) CreateHash(string secret)
+    {
+        var secretBytes = Encoding.UTF8.GetBytes(secret);
+        var salt = RandomNumberGenerator.GetBytes(_keyLength);
+        var hash = Rfc2898DeriveBytes.Pbkdf2(secretBytes, salt, _iterations, _hashAlgorithm, _keyLength);
+        return (hash, salt);
+    }
+
+    internal bool VerifyHash(string secret, byte[] hash, byte[] salt)
+    {
+        var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(secret, salt, _iterations, _hashAlgorithm, _keyLength);
+        return hashToCompare.SequenceEqual(hash);
+    }
+
+    internal static string BitConverterToString(byte[] bytes)
+    {
+        return BitConverter.ToString(bytes);
+    }
+
+    internal static byte[] BitConverterToBytes(string str)
+    {
+        return str.Split('-').Select(b => Convert.ToByte(b, 16)).ToArray();
+    }
+}
--- UserSecretsApp/Program.cs
+++ UserSecretsApp/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json;
-using System.Text.RegularExpressions;
 using Microsoft.Extensions.Configuration;
 using TextCopy;
+
+using UserSecretsApp;
 
 var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
@@ -14,16 +14,6 @@ var hashAlgorithm = new HashAlgorithmName(config["HashAlgorithm"]);
 var iterations = Convert.ToInt32(config["Iterations"]);
 var keyLength = Convert.ToInt32(config["KeyLength"]);
 var encryptionService = new EncryptionService(hashAlgorithm, iterations, keyLength);
-
-var s = new Secret("kk", "hh");
-try
-{
-    Console.WriteLine(s.ToString());
-}
-catch (Exception ex)
-{
-    // ignored
-}
 
 if (config["MasterHash"] == string.Empty)
 {
@@ -37,7 +27,6 @@ if (config["MasterHash"] == string.Empty)
 }
 
 Console.WriteLine("Welcome to the UserSecrets Vault!");
-Console.WriteLine();
 var masterSecret = ReadLine("Enter Master password: ", Console.Write);
 
 var masterHash = EncryptionService.BitConverterToBytes(config["MasterHash"]!);
@@ -73,7 +62,7 @@ while (verifiedMasterSecret && userSecretsFileInfo.Exists && !exit)
     if (secretContainer is null)
     {
         Console.WriteLine();
-        Console.WriteLine($"User key - {key} - not found.");
+        Console.WriteLine($"User key - {key} - not found...");
         var newKey = ReadLine("Enter New User Key: ", Console.Write);
         var newName = ReadLine("Enter New User Name: ", Console.Write);
         var newPassword = ReadLine("Enter New User Password: ", Console.Write);
@@ -100,10 +89,11 @@ while (verifiedMasterSecret && userSecretsFileInfo.Exists && !exit)
 
     if (secret is not null)
     {
+        Console.WriteLine("Secret was found...");
         await ClipboardService.SetTextAsync(secret.Password);
     }
     
-    Console.WriteLine("Exit: (y/n)");
+    Console.Write("Exit: (y/n)");
     exit = Console.ReadKey(true).Key is ConsoleKey.Y;
 }
 
@@ -114,11 +104,13 @@ var options = new JsonSerializerOptions
 
 var jsonString = JsonSerializer.Serialize(secretContainers, options);
 
+await ClipboardService.SetTextAsync("");
 await Task.Run(() => File.WriteAllText(userSecretsFileInfo.FullName, jsonString));
 
-Console.WriteLine("Saving file...");
-
 Console.WriteLine();
+Console.WriteLine("Saving file...");
+Console.WriteLine("Goodbye");
+
 
 static string ReadLine(string? message, Action<string> writer)
 {
@@ -128,77 +120,6 @@ static string ReadLine(string? message, Action<string> writer)
     }
 
     return Console.ReadLine() ?? string.Empty;
-}
-
-internal class EncryptionService
-{
-    private readonly HashAlgorithmName _hashAlgorithm;
-    private readonly int _iterations;
-    private readonly int _keyLength;
-
-    public EncryptionService(HashAlgorithmName hashAlgorithm, int iterations, int keyLength)
-    {
-        _hashAlgorithm = hashAlgorithm;
-        _iterations = iterations;
-        _keyLength = keyLength;
-    }
-
-    internal async Task<byte[]> EncryptAsync(string message, string secretKey, string salt)
-    {
-        using var aes = Aes.Create();
-        aes.Key = CreateDerivedKey(secretKey, Array.Empty<byte>(), _hashAlgorithm, _iterations, _keyLength);
-        aes.IV = BitConverterToBytes(salt);
-        var encryptor = aes.CreateEncryptor();
-        using var output = new MemoryStream();
-        await using var cryptoStream = new CryptoStream(output, encryptor, CryptoStreamMode.Write);
-        await cryptoStream.WriteAsync(Encoding.Unicode.GetBytes(message));
-        await cryptoStream.FlushFinalBlockAsync();
-        return output.ToArray();
-    }
-
-    internal async Task<string> DecryptAsync(byte[] encrypted, string secretKey, string salt)
-    {
-        using var aes = Aes.Create();
-        aes.Key = CreateDerivedKey(secretKey, Array.Empty<byte>(), _hashAlgorithm, _iterations, _keyLength);
-        aes.IV = BitConverterToBytes(salt);
-        var decryptor = aes.CreateDecryptor();
-        using var input = new MemoryStream(encrypted);
-        await using var cryptoStream = new CryptoStream(input, decryptor, CryptoStreamMode.Read);
-        using var output = new MemoryStream();
-        await cryptoStream.CopyToAsync(output);
-        return Encoding.Unicode.GetString(output.ToArray());
-    }
-
-    private static byte[] CreateDerivedKey(string secretKey, byte[] salt, HashAlgorithmName hashAlgorithm,
-        int iterations, int keyLength)
-    {
-        var secret = Encoding.Unicode.GetBytes(secretKey);
-        return Rfc2898DeriveBytes.Pbkdf2(secret, salt, iterations, hashAlgorithm, keyLength);
-    }
-
-    internal (byte[] Hash, byte[] Salt) CreateHash(string secret)
-    {
-        var secretBytes = Encoding.UTF8.GetBytes(secret);
-        var salt = RandomNumberGenerator.GetBytes(_keyLength);
-        var hash = Rfc2898DeriveBytes.Pbkdf2(secretBytes, salt, _iterations, _hashAlgorithm, _keyLength);
-        return (hash, salt);
-    }
-
-    internal bool VerifyHash(string secret, byte[] hash, byte[] salt)
-    {
-        var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(secret, salt, _iterations, _hashAlgorithm, _keyLength);
-        return hashToCompare.SequenceEqual(hash);
-    }
-
-    internal static string BitConverterToString(byte[] bytes)
-    {
-        return BitConverter.ToString(bytes);
-    }
-
-    internal static byte[] BitConverterToBytes(string str)
-    {
-        return str.Split('-').Select(b => Convert.ToByte(b, 16)).ToArray();
-    }
 }
 
 internal record Secret(string User, string Password);
