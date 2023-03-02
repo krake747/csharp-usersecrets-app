ef7be1966b8c57458806c0542f50ddbd6e8829dc
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-03-02 20:44:12 +0100

    🔊 logs(app): add logging functionality to app

--- UserSecretsApp/Program.cs
+++ UserSecretsApp/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System.Security.Cryptography;
 using System.Text.Json;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 using TextCopy;
-
 using UserSecretsApp;
 
-var config = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json")
-    .AddUserSecrets<Program>()
-    .Build();
+
+var config = CreateConfig();
+var log = CreateLogger(config);
+
+log.Information("Hello, UserSecretsApp!");
 
 var hashAlgorithm = new HashAlgorithmName(config["HashAlgorithm"]);
 var iterations = Convert.ToInt32(config["Iterations"]);
@@ -17,6 +18,7 @@ var encryptionService = new EncryptionService(hashAlgorithm, iterations, keyLeng
 
 if (config["MasterHash"] == string.Empty)
 {
+    log.Information("Creating new Master Hash");
     var newMasterSecret = ReadLine("Enter new Master Password: ", Console.Write);
     var (newHash, newSalt) = encryptionService.CreateHash(newMasterSecret);
 
@@ -26,7 +28,8 @@ if (config["MasterHash"] == string.Empty)
     Environment.Exit(0);
 }
 
-Console.WriteLine("Welcome to the UserSecrets Vault!");
+log.Information("Master Hash was found");
+
 var masterSecret = ReadLine("Enter Master password: ", Console.Write);
 
 var masterHash = EncryptionService.BitConverterToBytes(config["MasterHash"]!);
@@ -36,81 +39,119 @@ var verifiedMasterSecret = encryptionService.VerifyHash(masterSecret, masterHash
 
 if (!verifiedMasterSecret)
 {
+    log.Information("Master Secret was not able to be verified");
     Console.WriteLine("Unable to verify Master Secret!");
     Environment.Exit(0);
 }
 
+log.Information("Master Secret was able to be verified");
+Console.WriteLine("Welcome to the UserSecrets Vault!");
+
 var currentDir = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\Vault\"));
+
+if (!currentDir.Exists)
+{
+    log.Information("Vault directory was created");
+    currentDir.Create();
+}
+
+log.Information("Vault directory exists");
 var userSecretsFileInfo = new FileInfo(Path.Combine(currentDir.FullName, "secrets-container.json"));
 
 if (!userSecretsFileInfo.Exists)
 {
+    log.Information("Secret container JSON file was created");
     userSecretsFileInfo.Create().Dispose();
 }
 
+log.Information("Secret container JSON file exists");
 var json = await File.ReadAllTextAsync(userSecretsFileInfo.FullName);
 var secretContainers = json != string.Empty
-    ? JsonSerializer.Deserialize<List<SecretContainer>>(json)! 
+    ? JsonSerializer.Deserialize<List<SecretContainer>>(json)!
     : new List<SecretContainer>();
 
 var exit = false;
-while (!exit)
+do
 {
     var key = ReadLine("Enter User Key: ", Console.Write);
     var secretContainer = secretContainers.FirstOrDefault(u => u.Key == key);
 
     if (secretContainer is null)
     {
+        log.Information("Secret container was not found");
         Console.WriteLine();
         Console.WriteLine($"User key - {key} - not found...");
         var newKey = ReadLine("Enter New User Key: ", Console.Write);
         var newName = ReadLine("Enter New User Name: ", Console.Write);
         var newPassword = ReadLine("Enter New User Password: ", Console.Write);
+
+        log.Information("New Secret was created");
         var newSecret = new Secret(newName, newPassword);
-        var newJsonSecret = JsonSerializer.Serialize(newSecret); 
+        var newJsonSecret = JsonSerializer.Serialize(newSecret);
 
         var rngSalt = RandomNumberGenerator.GetBytes(keyLength);
         var salt = EncryptionService.BitConverterToString(rngSalt);
         var encrypted = await encryptionService.EncryptAsync(newJsonSecret, newKey, salt);
         var newEncryptedSecret = EncryptionService.BitConverterToString(encrypted);
         var newSecretContainer = new SecretContainer(newKey, salt, newEncryptedSecret);
-        Console.WriteLine("New Secret Container added...");
+
+        log.Information("New Secret container was added");
+        Console.WriteLine("New Secret container added...");
         Console.WriteLine();
-        //Console.WriteLine($"Encrypted data: {newEncryptedSecret}");
 
         secretContainers.Add(newSecretContainer);
         continue;
     }
 
+    log.Information("Secret container was found");
     var decryptedSecret = EncryptionService.BitConverterToBytes(secretContainer.EncryptedSecret);
     var decrypted = await encryptionService.DecryptAsync(decryptedSecret, secretContainer.Key, secretContainer.Salt);
     var secret = JsonSerializer.Deserialize<Secret>(decrypted);
-    //Console.WriteLine($"Decrypted data: {decrypted}");
 
     if (secret is not null)
     {
+        log.Information("Secret was found and copied to clipboard");
         Console.WriteLine("Secret was found...");
         await ClipboardService.SetTextAsync(secret.Password);
     }
-    
-    Console.Write("Exit: (y/n)");
-    exit = Console.ReadKey(true).Key is ConsoleKey.Y;
-}
 
-var options = new JsonSerializerOptions 
-{ 
+    Console.WriteLine("Exit: (y/n)");
+    exit = Console.ReadKey(true).Key is ConsoleKey.Y;
+} while (!exit);
+
+var options = new JsonSerializerOptions
+{
     WriteIndented = true
 };
 
 var jsonString = JsonSerializer.Serialize(secretContainers, options);
 
+log.Information("Clearing clipboard");
 await ClipboardService.SetTextAsync("");
-await Task.Run(() => File.WriteAllText(userSecretsFileInfo.FullName, jsonString));
 
 Console.WriteLine();
+
 Console.WriteLine("Saving file...");
+File.WriteAllText(userSecretsFileInfo.FullName, jsonString);
+
+log.Information("Goodbye - END");
 Console.WriteLine("Goodbye");
 
+
+static IConfiguration CreateConfig()
+{
+    return new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .AddUserSecrets<Program>()
+        .Build();
+}
+
+static ILogger CreateLogger(IConfiguration config)
+{
+    return new LoggerConfiguration()
+        .ReadFrom.Configuration(config)
+        .CreateLogger();
+}
 
 static string ReadLine(string? message, Action<string> writer)
 {
